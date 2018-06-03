213684ab5ae459b28f7a6039904d679c5bbee831
Mathieu Maes <mathieu@zimmo.be>
2018-06-03 15:35:07 +0200

    Fix tests

--- test/index.js
+++ test/index.js
@@ -2,10 +2,22 @@ var fs = require('fs');
 var expect = require('chai').expect;
 var supertest = require('supertest');
 var robots = require('../');
+var express = require('express');
+var app;
 
 describe('express-robots', function() {
+  function createSuperTest(robots) {
+    var app = express();
+    app.use(robots);
+    return supertest(app);
+  }
+  beforeEach(function () {
+    app = express();
+
+  });
   it('should work', function(done) {
-    var request = supertest(robots({UserAgent: '*', Disallow: '/'}));
+    app.use(robots());
+    var request = createSuperTest(robots({UserAgent: '*', Disallow: '/'}));
     request
       .get('/robots.txt')
       .end(function(err, res) {
@@ -17,7 +29,7 @@ describe('express-robots', function() {
   });
 
   it('should work with a crawl delay', function(done) {
-    var request = supertest(robots({UserAgent: '*', CrawlDelay: '5'}));
+    var request = createSuperTest(robots({UserAgent: '*', CrawlDelay: '5'}));
     request
       .get('/robots.txt')
       .end(function(err, res) {
@@ -29,7 +41,7 @@ describe('express-robots', function() {
   });
 
   it('should work with multiple crawl delays', function(done) {
-    var request = supertest(robots([
+    var request = createSuperTest(robots([
       {UserAgent: '*', CrawlDelay: '5'},
       {UserAgent: 'Foo', CrawlDelay: '10'}
     ]));
@@ -46,7 +58,7 @@ describe('express-robots', function() {
 
   it('should work with a sitemap', function(done) {
     var Sitemap = 'https://nowhere.com/sitemap.xml';
-    var request = supertest(robots({UserAgent: '*', Sitemap: Sitemap}));
+    var request = createSuperTest(robots({UserAgent: '*', Sitemap: Sitemap}));
     request
       .get('/robots.txt')
       .end(function(err, res) {
@@ -60,7 +72,7 @@ describe('express-robots', function() {
   it('should work with multiple sitemaps', function(done) {
     var Sitemaps = ['https://nowhere.com/sitemap.xml', 'https://nowhere.com/sitemap2.xml'];
 
-    var request = supertest(robots({UserAgent: '*', Sitemap: Sitemaps}));
+    var request = createSuperTest(robots({UserAgent: '*', Sitemap: Sitemaps}));
     request
       .get('/robots.txt')
       .end(function(err, res) {
@@ -74,7 +86,7 @@ describe('express-robots', function() {
   it('should work with sitemaps in multiple configs', function(done) {
     var Sitemaps = ['https://nowhere.com/sitemap.xml', 'https://nowhere.com/sitemap2.xml'];
 
-    var request = supertest(robots([{UserAgent: '*', Sitemap: Sitemaps[0]}, {UserAgent: 'Foo', Sitemap: Sitemaps[1]}]));
+    var request = createSuperTest(robots([{UserAgent: '*', Sitemap: Sitemaps[0]}, {UserAgent: 'Foo', Sitemap: Sitemaps[1]}]));
     request
       .get('/robots.txt')
       .end(function(err, res) {
@@ -88,7 +100,7 @@ describe('express-robots', function() {
   it('should work with multiple sitemaps in multiple configs', function(done) {
     var Sitemaps = ['https://nowhere.com/sitemap.xml', 'https://nowhere.com/sitemap2.xml'];
 
-    var request = supertest(robots([{UserAgent: '*', Sitemap: Sitemaps}, {UserAgent: 'Foo', Sitemap: Sitemaps}]));
+    var request = createSuperTest(robots([{UserAgent: '*', Sitemap: Sitemaps}, {UserAgent: 'Foo', Sitemap: Sitemaps}]));
     request
       .get('/robots.txt')
       .end(function(err, res) {
@@ -100,7 +112,7 @@ describe('express-robots', function() {
   });
 
   it('should work with files', function() {
-    var request = supertest(robots(__dirname + '/fixtures/robots.txt'));
+    var request = createSuperTest(robots(__dirname + '/fixtures/robots.txt'));
     request
       .get('/robots.txt')
       .end(function(err, res) {
@@ -110,7 +122,7 @@ describe('express-robots', function() {
   });
 
   it('should respond with an empty file if nothing is specified', function() {
-    var request = supertest(robots());
+    var request = createSuperTest(robots());
     request
       .get('/robots.txt')
       .end(function(err, res) {
